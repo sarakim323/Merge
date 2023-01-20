2e295a495cfcf23aeebcd9056ee312da6c84f4aa
Sara Kim <sarakim323@gmail.com>
2023-01-20 05:12:04 -0600

    delete feature for health screenings added

--- screens/HealthScreenings/HealthScreeningsEntryScreen.js
+++ screens/HealthScreenings/HealthScreeningsEntryScreen.js
@@ -5,10 +5,7 @@ import {FlatList, KeyboardAvoidingView, Modal, StyleSheet, Text, TextInput, View
 // import {auth} from '.../firebase';
 
 const HealthScreeningsEntry = () => {
-  const [data, setData] = useState(entries);
-  const [isRender, setisRender] = useState(false);
-  const [isModalVisible, setisModalVisible] = useState(false);
-  const [inputText, setinputText] = useState();
+  const [data, setData] = useState([]);
   const route = useRoute();
 
   const title = route.params.title;
@@ -41,68 +38,61 @@ const HealthScreeningsEntry = () => {
   // useEffect(() => {
   //   GetHealthScreenings();
   // }, [entries])
-
   const entries = {
     Medical: [
-      {date: '03/01/2020', name: 'Annual Physical Exam', provider: 'Dr. Shahina Shah, MD', notes: 'lab result - low TSH (0.32 mIU/L'},
-      {date: '06/15/2020', name: 'Thyroid F/U', provider: 'Dr. Shahina Shah, MD', notes: 'lab result - TSH (3.8 mIU/L'},
-      {date: '03/22/2021', name: 'Annual Physical Exam', provider: 'Dr. Shahina Shah, MD', notes: ''},
-      {date: '04/09/2021', name: 'COVID-19 Vaccination', provider: 'CVS', notes: 'first dose'}
+      {id: 1, date: '03/01/2020', name: 'Annual Physical Exam', provider: 'Dr. Shahina Shah, MD', notes: 'lab result - low TSH (0.32 mIU/L'},
+      {id: 2, date: '06/15/2020', name: 'Thyroid F/U', provider: 'Dr. Shahina Shah, MD', notes: 'lab result - TSH (3.8 mIU/L'},
+      {id: 3, date: '03/22/2021', name: 'Annual Physical Exam', provider: 'Dr. Shahina Shah, MD', notes: ''},
+      {id: 4, date: '04/09/2021', name: 'COVID-19 Vaccination', provider: 'CVS', notes: 'first dose'}
     ],
     Dental: [
-      {date: '05/31/2022', name: 'Routine Dental Exam & Cleaning', provider: 'Dr. Mary Smith', notes: ''},
-      {date: '01/01/2023', name: 'Invisalign Follow Up', provider: 'Dr. Danny Moon', notes: '1 out of 8 trays'},
-      {date: '01/15/2023', name: 'Invisalign Follow Up', provider: 'Dr. Danny Moon', notes: '2 out of 8 trays'},
+      {id: 5, date: '05/31/2022', name: 'Routine Dental Exam & Cleaning', provider: 'Dr. Mary Smith', notes: ''},
+      {id: 6, date: '01/01/2023', name: 'Invisalign Follow Up', provider: 'Dr. Danny Moon', notes: '1 out of 8 trays'},
+      {id: 7, date: '01/15/2023', name: 'Invisalign Follow Up', provider: 'Dr. Danny Moon', notes: '2 out of 8 trays'},
     ],
     Vision: [
-      {date: '12/13/2021', name: 'Routine Eye Exam', provider: 'Dr. Tiffany Doan', notes: 'p/u trial contacts by 12/20'},
-      {date: '12/18/2022', name: 'Routine Eye Exam', provider: 'Dr. Tiffany Doan', notes: ''},
+      {id: 8, date: '12/13/2021', name: 'Routine Eye Exam', provider: 'Dr. Tiffany Doan', notes: 'p/u trial contacts by 12/20'},
+      {id: 9, date: '12/18/2022', name: 'Routine Eye Exam', provider: 'Dr. Tiffany Doan', notes: ''},
     ],
    'Women\'s Wellness': [
-      {date: '08/31/2022', name: 'Routine Well-Woman Exam', provider: 'Dr. Samantha Carson', notes: ''},
+      {id: 10, date: '08/31/2022', name: 'Routine Well-Woman Exam', provider: 'Dr. Samantha Carson', notes: ''},
     ],
     Other: [
-      {date: '01/29/2022', name: 'Microneedling', provider: 'Avia Medical Spa', notes: '1 out of 3 rounds'},
-      {date: '02/29/2022', name: 'Hydrafacial', provider: 'Avia Medical Spa', notes: ''},
-      {date: '03/27/2022', name: 'Microneedling', provider: 'Avia Medical Spa', notes: '2 out of 3 rounds'},
-      {date: '04/30/2022', name: 'Hydrafacial', provider: 'Avia Medical Spa', notes: ''},
-      {date: '05/30/2022', name: 'Microneedling', provider: 'Avia Medical Spa', notes: '3 out of 3 rounds'},
-      {date: '09/19/2022', name: 'Dermaplaning', provider: 'Atomic Beauty', notes: ''},
-      {date: '01/03/2023', name: 'Microdermabrasion', provider: 'Atomic Beauty', notes: ''},
+      {id: 11, date: '01/29/2022', name: 'Microneedling', provider: 'Avia Medical Spa', notes: '1 out of 3 rounds'},
+      {id: 12, date: '02/29/2022', name: 'Hydrafacial', provider: 'Avia Medical Spa', notes: ''},
+      {id: 13, date: '03/27/2022', name: 'Microneedling', provider: 'Avia Medical Spa', notes: '2 out of 3 rounds'},
+      {id: 14, date: '04/30/2022', name: 'Hydrafacial', provider: 'Avia Medical Spa', notes: ''},
+      {id: 15, date: '05/30/2022', name: 'Microneedling', provider: 'Avia Medical Spa', notes: '3 out of 3 rounds'},
+      {id: 16, date: '09/19/2022', name: 'Dermaplaning', provider: 'Atomic Beauty', notes: ''},
+      {id: 17, date: '01/03/2023', name: 'Microdermabrasion', provider: 'Atomic Beauty', notes: ''},
     ]
   }
 
+  useState(() => {
+    const filteredentries = entries[title];
+    setData(filteredentries);
+  }, [])
+
   const addEntry = () => {
+    setData([...data, {key: data.date}]);
+  }
 
-  };
-
-  const onPressSaveEdit = () => {
-
-  };
-
-  const onPressItem = () => {
-    setisModalVisible(true);
-    setinputText(item.text);
+  const deleteEntry = (id) => {
+    setData((prev) => {
+      return prev.filter(entry => entry.id != id)
+    })
   }
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <FlatList data={entries[title]} keyExtractor={(item) => item.data} extraData={isRender} renderItem={({item}) =>
-        <TouchableOpacity style={styles.entryContainer}>
+      <Text style={styles.description}>Click on the entry to delete!</Text>
+      <FlatList data={data} keyExtractor={(item) => item.id} renderItem={({item}) =>
+        <TouchableOpacity style={styles.entryContainer} onPress={() => deleteEntry(item.id)} >
           <Text style={styles.item}>{item.name}</Text>
           <Text style={styles.item}>{item.date}</Text>
           <Text style={styles.item}>{item.provider}</Text>
           <Text style={styles.item}>{item.notes}</Text>
         </TouchableOpacity>
-      }/>
-      <Modal animationType='fade' visible={isModalVisible} onRequestClose={() => setisModalVisible(false)}>
-        <View style={styles.modalView}>
-          <Text style={styles.text}>Change Text: </Text>
-          <TextInput style={styles.textInput} onChangeText={(text) => setinputText(text)} defaultValue={inputText} editable={true} multiline={false} maxLength={200} />
-          <TouchableOpacity onPress={() => onPressSaveEdit()} style={styles.touchableSave}>
-            <Text style={styles.text}>Save</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
+      } />
       <TouchableOpacity onPress={addEntry} style={styles.button}>
           <Text style={styles.buttonText}>Add Entry</Text>
       </TouchableOpacity>
@@ -153,6 +143,12 @@ const styles = StyleSheet.create({
     color: '#FAF9F6',
     fontWeight: '700',
     fontSize: 16,
+  },
+  description: {
+    color: 'black',
+    fontWeight: '500',
+    fontSize: 20,
+    marginTop: 10
   },
   modalView: {
     flex: 1,
